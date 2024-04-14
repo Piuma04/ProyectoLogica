13c19be3476f0a98a3345782e455e5b4b0c65af1
Basher259 <baltasarschwerdt@gmail.com>
2024-04-14 19:03:38 -0300

    searchColumn implementado

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -30,19 +30,20 @@ put(Content, [RowN, ColN], _RowsClues, _ColsClues, Grid, NewGrid, 1, 1):-
 	put(Content, [RowN, ColN], _RowsClues, _ColsClues, Grid, NewGrid, 1, 0).
 
 %searchRightRowList(3,[[1,2],[3],[4,5,6],[7],[8]],X).
-searchRightRowList(0,[X|_Xs],X).
-searchRightRowList(Index,[_X|Xs],Elem):- Index > 0, NewIndex is Index-1, searchRightRowList(NewIndex,Xs,Elem).
+searchClueIndex(0,[X|_Xs],X).
+searchClueIndex(Index,[_X|Xs],Elem):- Index > 0, NewIndex is Index-1, searchClueIndex(NewIndex,Xs,Elem).
 
 
 %checkClues(+RowClues,+Row,+CurrentRowClue).
 checkClues([],[],0).
-
 
 checkClues(X,["#"|R],N):- N>0, NewN is N-1, checkClues(X, R, NewN).
 checkClues(X,[E|R],0):-E\="#", checkClues(X,R,0).
 
 checkClues([X|Xr],R,0):-checkClues(Xr,R,X).
 checkClues([],[E|R],0):-E\="#", checkClues([],R,0).
+searchColumn(_Coln, [], []).
+searchColumn(Coln,[X|Xs],[Y|Ys]):-searchClueIndex(Coln,X,Y),searchColumn(Coln,Xs,Ys).
 
 put(Content, [RowN, ColN], RowsClues, _ColsClues, Grid, NewGrid, 1, 0):-
 	
@@ -53,12 +54,18 @@ put(Content, [RowN, ColN], RowsClues, _ColsClues, Grid, NewGrid, 1, 0):-
 		;
 	replace(_Cell, ColN, Content, Row, NewRow)),
 
-	searchRightRowList(RowN, RowClues, RowClueList),
-	checkClues(RowClueList, ).
-
-	.
-put(Content, [RowN, ColN], _RowsClues, _ColsClues, Grid, NewGrid, 0, 1).
-
+	searchClueIndex(RowN, RowClues, RowClueList),
+	checkClues(RowClueList, NewRow,0).
+put(Content, [RowN, ColN], _RowsClues, ColsClues, Grid, NewGrid, 0, 1):-
+replace(Row, RowN, NewRow, Grid, NewGrid),
+	
+	(replace(Cell, ColN, _, Row, NewRow),
+	Cell == Content
+		;
+	replace(_Cell, ColN, Content, Row, NewRow)),
+	searchColumn(Coln,NewGrid,NewColumn),
+	searchClueIndex(ColN, ColsClues, ColsClueList),
+	checkClues(ColsClueList, NewColumn,0).
 put(Content, [RowN, ColN], _RowsClues, _ColsClues, Grid, NewGrid, 0, 0):-
 	% NewGrid is the result of replacing the row Row in position RowN of Grid by a new row NewRow (not yet instantiated).
 	replace(Row, RowN, NewRow, Grid, NewGrid),
